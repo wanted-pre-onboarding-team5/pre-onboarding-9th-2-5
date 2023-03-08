f6ecdc99feebd385d683cdbd415e68b4ad9da0e1
Jayun <jay.lee.2280@gmail.com>
2023-03-08 23:17:16 +0900

    [#2] Refactor: Error page 스타일링 및 메인 페이지로 가는 버튼 추가

--- src/pages/Error/index.tsx
+++ src/pages/Error/index.tsx
@@ -1,5 +1,9 @@
+import { Heading, Button, Flex } from '@chakra-ui/react';
 import { useRouteError } from 'react-router-dom';
 
+import { PATH_ROUTE } from '@/constants';
+
+import { useMovePage } from '@/hooks/useMovePage';
 interface ErrorType {
   statusText?: string;
   message?: string;
@@ -9,13 +13,19 @@ export const Error = () => {
   const error = useRouteError() as ErrorType;
   console.error(error);
 
+  const [goMain] = useMovePage([PATH_ROUTE.main]);
+
+  const handleButtonClick = () => {
+    goMain();
+  };
+
   return (
-    <div>
-      <h1>Oops!</h1>
-      <p>Sorry, an unexpected error has occurred.</p>
+    <Flex flexDirection='column' alignItems='center' gap='5' p='100'>
+      <Heading>에러가 발생했어요!</Heading>
       <p>
         <i>{error.statusText || error.message}</i>
       </p>
-    </div>
+      <Button onClick={handleButtonClick}>메인 페이지로 가기</Button>
+    </Flex>
   );
 };
