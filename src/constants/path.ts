93091655c70f681e69bbd8c12972a639dab126d5
letsjo <hyunoh.jo@gmail.com>
2023-03-09 07:39:05 +0900

    [#2] Feat: Path 상수값 추가

---
+++ src/constants/path.ts
@@ -0,0 +1,10 @@
+export const PATH_ROUTE = Object.freeze({
+  root: '/',
+  main: '/main',
+  reservations: '/reservations',
+  any: '/*',
+});
+
+export const PATH_API = Object.freeze({
+  data: './src/mocks/data.json',
+});
