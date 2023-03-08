7b11830d713eb938cc5c4ae0a07e783f9f6b4067
junsu1220 <crab7157@gmail.com>
2023-03-09 00:51:33 +0900

    [#4] Feat: travelApi 생성
    mocks의 data.json으로부터 데이터를 가져오는 함수를 구현했습니다.

---
+++ src/apis/travelApi.ts
@@ -0,0 +1,14 @@
+import { Product } from './travelApi.type';
+
+import { PATH_API } from '@/constants/path';
+
+export const getProductList = async (): Promise<Product[]> => {
+  try {
+    const response = await fetch(PATH_API.product);
+    const data = await response.json();
+    return data;
+  } catch (error) {
+    console.error('fetch error: ', error);
+    throw error;
+  }
+};
---
+++ src/apis/travelApi.type.ts
@@ -0,0 +1,10 @@
+export interface Product {
+  idx: number;
+  name: string;
+  mainImage: string;
+  description: string;
+  spaceCategory: string;
+  place: number;
+  maximumPurchases: number;
+  registrationDate: string;
+}
