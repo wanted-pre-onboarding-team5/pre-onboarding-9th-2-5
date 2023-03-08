7bf42fa8f57075999ff4107b6fc36317d76706d0
Jayun <jay.lee.2280@gmail.com>
2023-03-08 13:02:23 +0900

    [#8] Feat: `/` 경로로 진입 시 `/main`으로 redirect하도록 구현

--- src/router/index.tsx
+++ src/router/index.tsx
@@ -3,12 +3,18 @@ import { createBrowserRouter } from 'react-router-dom';
 import { PATH_ROUTE } from '@/constants';
 import { Main, Reservations, Error, Root } from '@/pages';
 
+import { rootLoader } from './loaders';
+
 const routes = [
   {
     path: PATH_ROUTE.root,
     element: <Root />,
     errorElement: <Error />,
     children: [
+      {
+        index: true,
+        loader: rootLoader,
+      },
       {
         path: PATH_ROUTE.main,
         element: <Main />,
