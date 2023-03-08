c078cea074dc2a20ba173fcfbfb1f16f43d90f54
letsjo <hyunoh.jo@gmail.com>
2023-03-09 07:35:35 +0900

    [#2] Feat: 로딩 페이지 구현

---
+++ src/pages/Loading/index.tsx
@@ -0,0 +1,11 @@
+import { Flex, Spinner } from '@chakra-ui/react';
+
+const Loading = () => {
+  return (
+    <Flex>
+      <Spinner thickness='4px' speed='0.65s' emptyColor='gray.200' color='blue.500' size='xl' />
+    </Flex>
+  );
+};
+
+export default Loading;
