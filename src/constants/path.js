8e014f753e664fbf7689648dff9fa2b3d31608ce
junsu1220 <crab7157@gmail.com>
2023-03-09 00:13:21 +0900

    [Refactor]: path.js ts로 변경
    path의 확장자를 ts로 변경했습니다.
    내부에 type을 지정해줬습니다.

--- src/constants/path.js
+++
@@ -1,7 +0,0 @@
-export const PATH_ROUTE = Object.freeze({
-  main: '/main',
-});
-
-export const PATH_API = Object.freeze({
-  product: '@/mocks/data.json',
-});
---
+++ src/constants/path.ts
@@ -0,0 +1,7 @@
+export const PATH_ROUTE: Readonly<{ [key: string]: string }> = Object.freeze({
+  main: '/main',
+});
+
+export const PATH_API: Readonly<{ [key: string]: string }> = Object.freeze({
+  product: '@/mocks/data.json',
+});
