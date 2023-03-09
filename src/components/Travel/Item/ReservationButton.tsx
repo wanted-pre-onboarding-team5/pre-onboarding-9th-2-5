323500d9581de619cc91c693bb64c91aa96d7bce
Jayun <jay.lee.2280@gmail.com>
2023-03-09 17:18:57 +0900

    [#10] Feat: 상품 예약버튼 클릭 시 장바구니에 저장되는 갯수를 1로 초기화하는 로직 구현

--- src/components/Travel/Item/ReservationButton.tsx
+++ src/components/Travel/Item/ReservationButton.tsx
@@ -5,12 +5,18 @@ import { getLocalStorageItem, setLocalStorageItem } from '@/utils';
 import { RESERVATIONS_KEY } from '@/constants/storage';
 
 export const ReservationButton = ({ itemData }) => {
-  const { idx } = itemData;
+  const { idx, name } = itemData;
 
   const handleReservationClick = () => {
     const reservations = getLocalStorageItem(RESERVATIONS_KEY) || [];
     const filteredReservations = reservations.filter((reservation) => reservation.idx !== idx);
-    setLocalStorageItem(RESERVATIONS_KEY, [...filteredReservations, itemData]);
+    if (filteredReservations.length < reservations.length) {
+      alert('이미 장바구니에 있는 상품이에요. 장바구니를 확인해 주세요.');
+      return;
+    }
+    const reservation = { ...itemData, quantity: 1 };
+    setLocalStorageItem(RESERVATIONS_KEY, [...filteredReservations, reservation]);
+    alert(`장바구니에 [${name}] 상품이 잘 담겼어요!`);
   };
 
   return (
