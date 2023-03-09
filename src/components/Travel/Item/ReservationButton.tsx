ba00c0c3fbdd57dc5e6671d91813db82b312b430
Jayun <jay.lee.2280@gmail.com>
2023-03-09 20:10:40 +0900

    [#6] Refactor: 모달창의 예약버튼 클릭 시 toast 띄워지도록 변경

--- src/components/Travel/Item/ReservationButton.tsx
+++ src/components/Travel/Item/ReservationButton.tsx
@@ -1,22 +1,24 @@
-import { Button } from '@chakra-ui/react';
+import { Button, useToast } from '@chakra-ui/react';
 
+import { getToastCartSuccessOption, TOAST_CART_INFO_OPTION } from '@/constants';
 import { getLocalStorageItem, setLocalStorageItem } from '@/utils';
 
 import { RESERVATIONS_KEY } from '@/constants/storage';
 
 export const ReservationButton = ({ itemData, quantity }) => {
   const { idx, name } = itemData;
+  const toast = useToast();
 
   const handleReservationClick = () => {
     const reservations = getLocalStorageItem(RESERVATIONS_KEY) || [];
     const filteredReservations = reservations.filter((reservation) => reservation.idx !== idx);
     if (filteredReservations.length < reservations.length) {
-      alert('이미 장바구니에 있는 상품이에요. 장바구니를 확인해 주세요.');
+      toast(TOAST_CART_INFO_OPTION);
       return;
     }
     const reservation = { ...itemData, quantity };
     setLocalStorageItem(RESERVATIONS_KEY, [...filteredReservations, reservation]);
-    alert(`장바구니에 [${name}] 상품이 ${quantity}개 담겼어요!`);
+    toast(getToastCartSuccessOption(name, quantity));
   };
 
   return (
