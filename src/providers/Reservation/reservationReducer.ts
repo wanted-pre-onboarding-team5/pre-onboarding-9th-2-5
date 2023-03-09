5e5c14e986430dc53c41158e27cefda0ec05de11
Jayun <jay.lee.2280@gmail.com>
2023-03-09 17:40:06 +0900

    [#3] Refactor: reservationReducer의 중복 로직 제거

--- src/providers/Reservation/reservationReducer.ts
+++ src/providers/Reservation/reservationReducer.ts
@@ -1,14 +1,12 @@
-import { setLocalStorageItem, calcSubtotal, calcTotalItemCount } from '@/utils';
+import { setLocalStorageItem, updateReservationStatus } from '@/utils';
 
 import { RESERVATIONS_KEY } from '@/constants/storage';
 
-// TODO: 중복 로직 리팩토링
 export const reservationReducer = (state, action) => {
   switch (action.type) {
     case 'init': {
       const reservations = action.payload;
-      const subtotal = calcSubtotal(reservations);
-      const totalItemCount = calcTotalItemCount(reservations);
+      const { subtotal, totalItemCount } = updateReservationStatus(reservations);
 
       return {
         ...state,
@@ -22,8 +20,7 @@ export const reservationReducer = (state, action) => {
       const idx = action.payload;
       const updatedCart = cart.filter((item) => item.idx !== idx);
       setLocalStorageItem(RESERVATIONS_KEY, updatedCart);
-      const subtotal = calcSubtotal(updatedCart);
-      const totalItemCount = calcTotalItemCount(updatedCart);
+      const { subtotal, totalItemCount } = updateReservationStatus(updatedCart);
 
       return {
         ...state,
@@ -43,8 +40,7 @@ export const reservationReducer = (state, action) => {
         }
       });
       setLocalStorageItem(RESERVATIONS_KEY, updatedCart);
-      const subtotal = calcSubtotal(updatedCart);
-      const totalItemCount = calcTotalItemCount(updatedCart);
+      const { subtotal, totalItemCount } = updateReservationStatus(updatedCart);
 
       return {
         ...state,
@@ -65,8 +61,7 @@ export const reservationReducer = (state, action) => {
         }
       });
       setLocalStorageItem(RESERVATIONS_KEY, updatedCart);
-      const subtotal = calcSubtotal(updatedCart);
-      const totalItemCount = calcTotalItemCount(updatedCart);
+      const { subtotal, totalItemCount } = updateReservationStatus(updatedCart);
 
       return {
         ...state,
--- src/utils/cart.ts
+++ src/utils/cart.ts
@@ -1,13 +1,20 @@
-export const calcSubtotal = (cart) => {
+const calcSubtotal = (cart) => {
   return cart.reduce((acc, cur) => {
     acc += cur.quantity * cur.price;
     return acc;
   }, 0);
 };
 
-export const calcTotalItemCount = (cart) => {
+const calcTotalItemCount = (cart) => {
   return cart.reduce((acc, cur) => {
     acc += cur.quantity;
     return acc;
   }, 0);
+};
+
+export const updateReservationStatus = (cart) => {
+  const subtotal = calcSubtotal(cart);
+  const totalItemCount = calcTotalItemCount(cart);
+
+  return { subtotal, totalItemCount };
 };
