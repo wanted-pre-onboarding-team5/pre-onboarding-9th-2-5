ace6ad902546625fda308334d4fd7e84497850d2
Seok-CH <tjr6677@gmail.com>
2023-03-09 14:02:58 +0900

    [#2] Refactor: 필터 로직을 함수 이름을 따로 만들어서 밖으로 빼냄

--- src/features/product-list/list-container.tsx
+++ src/features/product-list/list-container.tsx
@@ -2,31 +2,32 @@ import { VStack } from '@chakra-ui/react';
 import React from 'react';
 
 import { useFilterContext } from './filter-provider';
-import ListItem from './list-item';
+import ListItem, { MockItemType } from './list-item';
 
 import mockData from '@/mocks/data.json';
 
 function ListContainer() {
   const { filter } = useFilterContext();
+
+  // TODO: FilterStrategy를 기존의 Context와 연결해보자
+  const filterStrategy = (mockData: MockItemType) => {
+    for (const [key, value] of Object.entries(filter)) {
+      if (!filter[key]) continue;
+      if (key === 'price') {
+        const [min, max] = value.split('~');
+        if (mockData.price < Number(min) || mockData.price > Number(max)) return false;
+      } else {
+        if (filter.spaceCategory !== mockData.spaceCategory) return false;
+      }
+    }
+    return true;
+  };
+
   return (
     <VStack gap={'8px'}>
-      {mockData
-        .filter((mockData) => {
-          for (const [key, value] of Object.entries(filter)) {
-            if (!filter[key]) continue;
-
-            if (key === 'price') {
-              const [min, max] = value.split('~');
-              if (mockData.price < Number(min) || mockData.price > Number(max)) return false;
-            } else {
-              if (filter.spaceCategory !== mockData.spaceCategory) return false;
-            }
-          }
-          return true;
-        })
-        .map((mockItem) => (
-          <ListItem key={mockItem.idx} itemData={mockItem} />
-        ))}
+      {mockData.filter(filterStrategy).map((mockItem) => (
+        <ListItem key={mockItem.idx} itemData={mockItem} />
+      ))}
     </VStack>
   );
 }
