802853e55b542c2699a5856ec61028027080d22e
Jayun <jay.lee.2280@gmail.com>
2023-03-10 11:40:38 +0900

    [#27] Refactor: toast option에 Object.freeze 적용
    Co-authored-by: changhwan seok <chadseok@users.noreply.github.com>
    Co-authored-by: hannaax <hannaax@users.noreply.github.com>
    Co-authored-by: seunghoonKang <seunghoonKang@users.noreply.github.com>
    Co-authored-by: Tummy <letsjo@users.noreply.github.com>
    Co-authored-by: junsu park <junsu1220@users.noreply.github.com>

--- src/constants/toast.ts
+++ src/constants/toast.ts
@@ -1,15 +1,15 @@
-const TOAST_CART_OPTION = {
+const TOAST_CART_OPTION = Object.freeze({
   duration: 2000,
   isClosable: true,
   position: 'top-right',
-};
+});
 
-export const TOAST_CART_INFO_OPTION = {
+export const TOAST_CART_INFO_OPTION = Object.freeze({
   ...TOAST_CART_OPTION,
   title: '이미 장바구니에 있는 상품이에요.',
   description: '장바구니를 확인해 주세요.',
   status: 'info',
-};
+});
 
 export const getToastCartSuccessOption = (name: string, quantity: number) => {
   return {
