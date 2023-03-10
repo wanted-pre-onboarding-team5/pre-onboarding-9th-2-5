52377246dc05c34e4b2299c901caca6e77dc9254
Jayun <jay.lee.2280@gmail.com>
2023-03-10 19:30:46 +0900

    [#29] Deploy: gh-pages 패키지 설치 및 script 변경

--- vite.config.ts
+++ vite.config.ts
@@ -4,5 +4,6 @@ import tsconfigPaths from 'vite-tsconfig-paths';
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: '/pre-onboarding-9th-2-5/',
   plugins: [react(), tsconfigPaths()],
 });
